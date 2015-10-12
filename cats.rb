4388233304996796837244b403a4c25bcf4986ec
Regina <laulessr@gmail.com>
2015-10-12 14:30:03 -0500

    Made getter/setter into attr_

--- cats.rb
+++ cats.rb
@@ -1,31 +1,16 @@
-class Cats
-
-	def set_name=(cat_name)
-		@name = cat_name
-	end
-
-	def get_name
-		return @name
-	end
-
-	def set_owner=(owner_name)
-		@owner_name = owner_name
-	end
-
-	def get_owner
-		return @owner_name
-	end
-
-	def meow
-		return "meow!!!"
-	end
-
+class Pet
+  attr_writer :name, :owner_name
+  attr_reader :name, :owner_name  
 end
 
+class Cats < Pet
+  def meow	
+    return "meow"
+  end    
+end	
+
 my_cat = Cats.new
-
-my_cat.set_name="Chairman Meow"
-
-cat_name = my_cat.get_name
+my_cat.name = "Chairman Meow"
+cat_name = my_cat.name
 
 puts "#{cat_name} says #{my_cat.meow}"
