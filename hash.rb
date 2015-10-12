368e5018750c81b2d7373bddab0aab7bf1850745
Regina <laulessr@gmail.com>
2015-10-12 14:44:58 -0500

    hash initial commit

---
+++ hash.rb
@@ -0,0 +1,9 @@
+#hashes of entries for a prize
+entry_01 = { name: "Kelly", email: "Kelly@gmail.com" }
+entry_02 = { name: "Joanna", email: "jojo@jojo.net" }
+entry_03 = { name: "Carson", email: "deezy@gmail.com" }
+
+winner = entry_02[:name]
+
+puts "#{winner} is the winner of the Mac laptop!"
+
